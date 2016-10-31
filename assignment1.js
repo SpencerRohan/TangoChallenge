675252e9c18f789630c5cac429e75861e954f710
Spencer Rohan <spencermrohan@gmail.com>
2016-10-31 16:39:57 -0500

    Setup app.js file for module import

---
+++ assignment1.js
@@ -0,0 +1,17 @@
+function a1(){
+
+	console.log('Assignment 1 Alpha');
+
+};
+
+function a2(){
+
+	console.log('Assignment 1 Beta');
+
+};
+
+var assignment1A = a1,
+		assignment1B = a2;
+
+module.exports.a1a = assignment1A;
+module.exports.a1b = assignment1B;
