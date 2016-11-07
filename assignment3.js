70853c4730a12e7337a95c75d7d6e93383f0e307
Spencer Rohan <spencermrohan@gmail.com>
2016-11-07 02:47:49 -0600

    Removed brute force attempt

--- assignment3.js
+++ assignment3.js
@@ -24,34 +24,18 @@ assignment3.strToNumber = function(str) {
 * 
 * Ways to solve:
 *	- Randomize 7 characters out of letters and check against strToNumber until it equals 667734139064
-* - 
-* 
+* - Recursive find all 7 letter combinations and then test those to numbers?
+* - Divide 37 from hash to find index number - place letter in a new array - then join to produce string.
 *
 **/
 
-assignment3.getWord = function(length){
-	var letters = ["a","c","d","e","g","i","l","m","n","o","p","r","s","t","u","w"];
-	var word = [];
-	for (var i = 0; i < length; i++){
-    word[i] = letters[Math.floor(Math.random() * letters.length)];
-	}
-	word = word.join('');
-	return word;
-};
 
-assignment3.numberToStr = function(number) {
-	var length = 7;
-	var word = assignment3.getWord(length);
-	var count = 1;
-	while (assignment3.strToNumber(word) !== 667734139064 || assignment3.strToNumber(word) !== 680131662011){
-		word = assignment3.getWord(length);
-		count++
-	};
-	console.log(word);
-	console.log(count);
-};
 
-assignment3.numberToStr(1)
+assignment3.numberToStr = function(h) {
+    
+}
+
+console.log(assignment3.numberToStr(667734139064));
 
 
 module.exports.strToNumber = assignment3.strToNumber;
