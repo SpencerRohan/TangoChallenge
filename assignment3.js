8c4077eb1c36830ef43f848620c4737f4c058fb7
Spencer Rohan <spencermrohan@gmail.com>
2016-11-07 02:09:08 -0600

    Brute force answer for assignment-3

--- assignment3.js
+++ assignment3.js
@@ -20,9 +20,38 @@ assignment3.strToNumber = function(str) {
   return h;
 };
 
-assignment3.numberToStr = function(number) {
-	
+/** Pseudo Code
+* 
+* Ways to solve:
+*	- Randomize 7 characters out of letters and check against strToNumber until it equals 667734139064
+* - 
+* 
+*
+**/
+
+assignment3.getWord = function(length){
+	var letters = ["a","c","d","e","g","i","l","m","n","o","p","r","s","t","u","w"];
+	var word = [];
+	for (var i = 0; i < length; i++){
+    word[i] = letters[Math.floor(Math.random() * letters.length)];
+	}
+	word = word.join('');
+	return word;
 };
+
+assignment3.numberToStr = function(number) {
+	var length = 7;
+	var word = assignment3.getWord(length);
+	var count = 1;
+	while (assignment3.strToNumber(word) !== 667734139064 || assignment3.strToNumber(word) !== 680131662011){
+		word = assignment3.getWord(length);
+		count++
+	};
+	console.log(word);
+	console.log(count);
+};
+
+assignment3.numberToStr(1)
 
 
 module.exports.strToNumber = assignment3.strToNumber;
